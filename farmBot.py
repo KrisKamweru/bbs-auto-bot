28e305b147449f361b9bec9a066fa28b5c1db3a6
KrisKamweru <kriskamweru@gmail.com>
2020-08-19 19:20:54 +0300

    Added a few utility functions, as well as the function that gets from the home screen to story mode. Also added nore screenshots for use throughout the application

--- farmBot.py
+++ farmBot.py
@@ -1,3 +1,61 @@
+# =================================================
+# ===================== TODO ======================
+# =================================================
+# Determine all the scenarios bot should cover
+# -> Story farming
+# -> Crystal/Jewel/Ring farming
+# -> Scroll quests
+# -> Single player IZ farming 
+# -> Coop farming (for Inheritance Zone, Accessory Fusion Trials, Extreme Co-op and Epic Raids):
+# - => With Guild Invite
+# - => With Public Invite
+# - => With first guild invite, then public invite after a delay 
+#  
+# Determine what images are needed for all scenarios
+#  
+# Create function for getting from the game load screen to home screen
+# Create a function for getting from any screen to the home screen
+#
+# DONE:: Create click funtion
+# Create click and drag function for Epic Raids
+#  
+# Create function for getting from home screen to:
+# -> Story mode main
+# - => Normal
+# - => Hard 
+# 
+# -> Story mode side
+# - => Normal
+# - => Hard
+# 
+# -> Sub Stories
+#  
+# -> Single player farming:
+# - => Crystals
+# - => Jewels
+# - => Resurrection Rings
+# - => Scrolls
+# - => Inheritance Zone
+# 
+# -> Coop Farming:
+# - => Extreme Coop
+# - => Inheritance Zone
+# - => Extreme Coop
+# - => Accessory Fusion Trials 
+# 
+# Create Next quest function for story mode with ticket limit
+# Create retry function for:
+# -> Solo farming
+# -> Coop IZ, Ex Coop & Powder Farming
+# -> ER  
+
+# Function that accepts the x and y values of the top left corner of the found images, 
+# as well as the width and height of the found images, 
+# and uses the width and height to click in the center of the image
+
+# =================================================
+# ==================== IMPORTS ====================
+# =================================================
 from pyautogui import *
 import pyautogui
 import time
@@ -5,75 +63,28 @@ import keyboard
 import random
 import win32api, win32con
 
-time.sleep(4)
+# =================================================
+# =================== FUNCTIONS ===================
+# =================================================
 
-# The following code was used to test the button pushes on a 1920*1080 screen:
-# https://codepen.io/gregjha/pen/MWyemyB
-# it is from this pen that the button images are sourced
+# pyautogui.locateOnScreen(usePath, confidence = 0.8)
+# keyboard.is_pressed('q') == False
 
-# Function that accepts the x and y values of the top left corner of the found iamges, as well as the width and height of the found images, and uses the width and height to click in the center of the image
 def click(x,y, width, height):
     win32api.SetCursorPos((x + int(width/2),y + int(height/2)))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x + int(width/2),y + int(height/2), 0, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x + int(width/2),y + int(height/2), 0, 0)
 
-# Iteration variable for the click test
-iteration = 1
+def findAndClick(pathValue):
+    usePath = pathValue
+    coords = None
+    while coords == None:
+        coords = pyautogui.locateOnScreen(usePath, confidence = 0.8)
+    click(coords[0], coords[1], coords[2], coords[3])
+    time.sleep(1)
 
-# Click the two buttons in alternating fashion
-while keyboard.is_pressed('q') == False:    
-    if iteration == 1:
-        usePath = 'assets/html_button_accept.png'
-    else:
-        usePath = 'assets/html_button_cancel.png'
-    
-    find = pyautogui.locateOnScreen(usePath, confidence = 0.8)
-    
-    if find != None:
-        if iteration == 1:
-            iteration = 2
-        else:
-            iteration = 1
-        click(find[0], find[1], find[2], find[3])
-        time.sleep(1)
-        
-    else:
-        print("Not found!")
-        time.sleep(1)
+def homeToStory():
+    findAndClick("assets\homepage_quest_button.png")
+    findAndClick("assets\quests_story.jpg")
 
-# def startUp():
-#     while 1:
-#         if pyautogui.locateOnScreen('assets/homepage_quests.jpg', confidence=0.8) != None:
-#             print("found")
-#             sleep(3)
-#             x,y = pyautogui.locateOnScreen('assets/homepage_quests.jpg', confidence=0.8)
-#             click(x,y)
-#             return True
-
-# if pyautogui.locateOnScreen('assets/homepage_quests.jpg', confidence=0.8) != None:
-#     while 1:
-#         l = pyautogui.locateOnScreen('assets/homepage_quests.jpg', confidence=0.8)
-#         try:
-#             x = (pyautogui.locateOnScreen('assets/homepage_quests.jpg', confidence=0.8))[0]
-#             print(x)
-#             print(type(x))
-#             time.sleep(0.5)
-#         except TypeError:
-#             print("Not there")
-#         print(l)
-#         print(type(l))
-#         time.sleep(1)
-        
-
-#while keyboard.is_pressed('q') == False:
-#    retValue = False
-#
-#    while retValue == False:
-#        retValue = startUp()
-#    while 1:
-#        print("ay?")
-#        time.sleep(1)
-# while
-
-
-    
+homeToStory()
---
+++ findTest.py
@@ -0,0 +1,28 @@
+# =================================================
+# ==================== IMPORTS ====================
+# =================================================
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+# usePath = ""
+usePath = "assets\homepage_quest_button.png"
+
+# while keyboard.is_pressed('q') == False:
+#     if (pyautogui.locateAllOnScreen(usePath, confidence = 0.8) != None) or (pyautogui.locateAllOnScreen(usePath, confidence = 0.8) != ImageNotFoundException):
+#         print(list(pyautogui.locateAllOnScreen(usePath, confidence = 0.8)))
+#         time.sleep(1)
+#     else:
+#         print("Not found")
+#         time.sleep(1)
+
+# while keyboard.is_pressed('q') == False:
+#     if pyautogui.locateOnScreen(usePath, confidence = 0.8) != None:
+#         print(pyautogui.locateOnScreen(usePath, confidence = 0.8))
+#         time.sleep(1)
+#     else:
+#         print("Not found")
+#         time.sleep(1)
